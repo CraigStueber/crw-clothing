0f7fe28f653b0f12e8c89792622123fa5b57a9ce
Craig A Stueber <58899336+CraigStueber@users.noreply.github.com>
2020-05-04 21:13:55 -0400

    Add files via upload
    Added sign-in page. Starting to play with the sign-in function of it now. SOOO Excited.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component'
 import Header from './components/header/header.component'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
